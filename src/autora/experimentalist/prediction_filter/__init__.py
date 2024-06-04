6f8cb8f53019decd53cf964f6f2787031b27f753
Younes Strittmatter <younes_strittmatter@brown.edu>
2024-06-04 12:59:41 -0400

    update pypi name

--- src/autora/experimentalist/prediction_filter/__init__.py
+++ src/autora/experimentalist/prediction_filter/__init__.py
@@ -9,10 +9,10 @@ from sklearn.base import BaseEstimator
 
 
 def filter(
-    conditions: pd.DataFrame,
-    model: BaseEstimator,
-    filter_function: Callable,
-    reset_index: bool = True,
+        conditions: pd.DataFrame,
+        model: BaseEstimator,
+        filter_function: Callable,
+        reset_index: bool = True,
 ) -> pd.DataFrame:
     """
     Filter conditions based on the expected outcome io the mdeol
@@ -35,30 +35,40 @@ def filter(
 
         For the filter function, be aware of the output type of the predict function. For example,
         here, we expect a list with a single entry
-        >>> filter_fct = lambda x: 5 < x[0] < 10
+        >>> filter_fct = lambda x: 5 < x < 10
         >>> pool = pd.DataFrame({'x': [1, 2, 3, 4, 5, 6]})
         >>> filter(pool, model, filter_fct)
            x
         0  3
         1  4
 
+        >>> filter_fct_2d = lambda x: 4 < x[0] + x[1] < 10
+        >>> pool = np.array([[1, 0], [0, 1], [0, 1], [1 ,1], [2, 2]])
+        >>> model.predict(pool)
+        array([[3, 1],
+               [1, 3],
+               [1, 3],
+               [3, 3],
+               [5, 5]])
+
+        >>> filter(pool, model, filter_fct_2d)
+        array([[1, 1]])
 
     """
-    new_conditions = conditions.copy()
+    c_array = np.array(conditions)
+    if len(c_array.shape) == 1:
+        c_array = c_array.reshape(-1, 1)
 
-    def __filter(x):
-        y = model.predict(np.array(x))
-        _bool = filter_function(y)
-        return _bool
+    _pred = model.predict(c_array)
+    _filter = np.apply_along_axis(filter_function, 1, _pred)
+    _filter = _filter.reshape(1,-1)
 
-    new_conditions["__prediction"] = new_conditions.apply(__filter, axis=1)
+    new_conditions = c_array[list(_filter[0])]
 
-    _c = new_conditions[new_conditions["__prediction"]]
-    _c = _c.drop(columns=["__prediction"])
-    if reset_index:
-        _c.reset_index(drop=True, inplace=True)
+    if isinstance(conditions, pd.DataFrame):
+        new_conditions = pd.DataFrame(new_conditions, columns=conditions.columns)
 
-    return _c
+    return new_conditions
 
 
 prediction_filter = filter
